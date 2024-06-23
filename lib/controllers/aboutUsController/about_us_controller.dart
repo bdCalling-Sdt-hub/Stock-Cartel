a761e6f666032683037c8e8e84aadbd78a2ecd16
mrswapon <mrswapon15@gmail.com>
2024-06-23 16:05:23 +0600

    terms, privacy and about us completed

---
+++ lib/controllers/aboutUsController/about_us_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+import '../../services/api_client.dart';
+import '../../services/api_constants.dart';
+
+class AboutUsController extends GetxController {
+  RxBool isLoading = false.obs;
+  RxString content = ''.obs;
+
+//==============================> Get About Us Method <==========================
+  getAboutUs() async {
+    isLoading.value = true;
+    Map<String, String> header = {'Content-Type': 'application/json'};
+    var response = await ApiClient.getData(ApiConstants.getAboutUsEndPoint,
+        headers: header);
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var attributes = data['data']['attributes']['content'];
+      content.value = attributes;
+      isLoading.value = false;
+    }
+  }
+}
